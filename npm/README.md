e62f6cc8f85546b16df0c15f89ad8680e1814d54
smm-h <smm-h@users.noreply.github.com>
2026-06-16 19:36:46 +0200

    add npm name-squatter package placeholder
    Claude-Code-Session-Id: 9bca184e-78f9-4085-a618-7e006597aa5f

---
+++ npm/README.md
@@ -0,0 +1,8 @@
+# toolstream
+
+This is a name-claim shim. The real package is on PyPI:
+
+    pip install toolstream
+
+- PyPI: https://pypi.org/project/toolstream/
+- GitHub: https://github.com/smm-h/toolstream
---
+++ npm/index.js
@@ -0,0 +1,5 @@
+throw new Error(
+  "toolstream is a Python package. Install it with: pip install toolstream\n" +
+  "PyPI: https://pypi.org/project/toolstream/\n" +
+  "GitHub: https://github.com/smm-h/toolstream"
+);
